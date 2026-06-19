c243c12d5eaea3295d161d425453d3506d311082
Sreehitha Madiraju <masreehi41@gmail.com>
2026-06-19 22:05:13 +0530

    Update predict_alzhimers.py

--- Alzhimers/predict_alzhimers.py
+++ Alzhimers/predict_alzhimers.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+
 import torch
 import torch.nn as nn
 
 from torchvision import models, transforms
 from PIL import Image
+
+# --------------------------------
+# CLASSES
+# --------------------------------
 
 classes = [
     "Mild Demented",
@@ -11,7 +17,21 @@ classes = [
     "Very Mild Demented"
 ]
 
-model = models.resnet18(weights=None)
+# --------------------------------
+# PATHS
+# --------------------------------
+
+BASE_DIR = Path(__file__).resolve().parent
+
+model_path = BASE_DIR / "models" / "alzhimers_model.pth"
+
+# --------------------------------
+# LOAD MODEL
+# --------------------------------
+
+model = models.resnet18(
+    weights=None
+)
 
 model.fc = nn.Linear(
     model.fc.in_features,
@@ -20,33 +40,49 @@ model.fc = nn.Linear(
 
 model.load_state_dict(
     torch.load(
-        r"C:\Users\masre\OneDrive\Desktop\Mini Project Code\Alzhimers\models\alzhimers_model.pth",
+        model_path,
         map_location="cpu"
     )
 )
 
 model.eval()
 
+# --------------------------------
+# INPUT IMAGE
+# --------------------------------
+
 image_path = input(
     "Enter MRI image path: "
 ).strip('"')
 
 transform = transforms.Compose([
-    transforms.Resize((224,224)),
+    transforms.Resize(
+        (224, 224)
+    ),
     transforms.ToTensor()
 ])
 
 image = Image.open(
     image_path
-).convert("RGB")
+).convert(
+    "RGB"
+)
 
 image = transform(
     image
-).unsqueeze(0)
+).unsqueeze(
+    0
+)
+
+# --------------------------------
+# PREDICTION
+# --------------------------------
 
 with torch.no_grad():
 
-    output = model(image)
+    output = model(
+        image
+    )
 
     probabilities = torch.softmax(
         output,
@@ -62,6 +98,10 @@ with torch.no_grad():
         probabilities[0][prediction.item()]
         * 100
     )
+
+# --------------------------------
+# OUTPUT
+# --------------------------------
 
 print()
 

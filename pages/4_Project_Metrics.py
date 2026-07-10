0b3ee5a7fb0659ccf7b1d74da091f91b5be052d3
Sreehitha Madiraju <masreehi41@gmail.com>
2026-07-10 18:26:06 +0530

    Refactor load_metrics to remove threading
    Removed ThreadPoolExecutor for loading metrics and simplified the metric loading process.

--- pages/4_Project_Metrics.py
+++ pages/4_Project_Metrics.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from concurrent.futures import ThreadPoolExecutor
 
 from brain_tumor.evaluation import evaluate_model as evaluate_brain
 from Alzhimers.evaluation import evaluate_model as evaluate_alzheimer
@@ -20,32 +19,14 @@ st.set_page_config(
 @st.cache_data
 def load_metrics():
 
-    with ThreadPoolExecutor() as executor:
+    brain_metrics = evaluate_brain()
 
-        brain_future = executor.submit(
-            evaluate_brain
-        )
-
-        alzheimer_future = executor.submit(
-            evaluate_alzheimer
-        )
-
-        brain_metrics = brain_future.result()
-
-        alzheimer_metrics = alzheimer_future.result()
+    alzheimer_metrics = evaluate_alzheimer()
 
     return brain_metrics, alzheimer_metrics
 
 
-# -----------------------------------
-# LOAD METRICS
-# -----------------------------------
-
-with st.spinner(
-    "Calculating metrics, please wait..."
-):
-
-    brain_metrics, alzheimer_metrics = load_metrics()
+brain_metrics, alzheimer_metrics = load_metrics()
 
 # -----------------------------------
 # HEADER
